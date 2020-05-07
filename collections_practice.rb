da675fb32b3749e49ac0a4245240ef5e11288a2b
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-07 02:15:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,8 +13,16 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  
+  array1 =[]
   array.each_with_index do |num, i|
-    if i = 1
+    if i == 1
+      array1[2] = num
+    elsif i == 2
+      array1[1] = num
+    else
+      array1 << num
+    end
+  end
+    
       
 end
